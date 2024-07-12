2c58afb2563c8cb6df53fb38b329c17499950bc7
Simon Nolte <81624911+smnnlt@users.noreply.github.com>
2024-07-13 00:13:42 +0200

    Test retrival of header data

--- src/test.cpp
+++ src/test.cpp
@@ -1,9 +1,11 @@
 #include <Rcpp.h>
 #include "ezc3d.h"
+#include "Header.h"
 using namespace Rcpp;
+using namespace ezc3d;
 
 // [[Rcpp::export]]
 int read(const std::string &filepath) {
-  ezc3d::c3d c3d(filepath);
-  return 5;
+  ezc3d::c3d x = c3d(filepath);
+  return x.header().nbFrames();
 }
