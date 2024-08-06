3db235f38620ee4517f7bb8ec4d71324a1654d71
Simon Nolte <81624911+smnnlt@users.noreply.github.com>
2024-08-06 16:31:18 +0200

    Add point data reshape function

--- R/convert.R
+++ R/convert.R
@@ -3,20 +3,62 @@
 #' Get the point data in a data.frame
 #'
 #' @param x A c3d object, as imported by \code{c3d_read()}.
+#' @param format Either "wide" (default) or "long" to determine the format of the
+#'   resulting data frame. Long format has three columns per point (x,y,z),
+#'   short format has three rows per frame per point (x,y,z).
 #'
-#' @return A data.frame with three columns (x, y, z) per point. It has n rows
-#'   and k*3 columns, with n as the number of recorded frames and k as the
-#'   number of recorded points.
+#' @return A data.frame with the c3d point data. The structure of the data frame
+#'   depends on the 'format' argument.
 #' @export
 
-c3d_data <- function(x) {
+c3d_data <- function(x, format = "wide") {
   # change data format from nested list to data.frame
   out <- as.data.frame(
     matrix(unlist(x$data), nrow = x$header$nframes, byrow = TRUE)
   )
   # get label names
   colnames(out) <- paste0(rep(x$labels, each = 3), c("_x", "_y", "_z"))
-  out
+  if (format == "wide") {
+    out
+  } else if (format == "long") {
+    c3d_longer(out)
+  } else {
+    stop("'format' argument in c3d_data() must be either 'wide' or 'long'")
+  }
+}
+
+#' Convert wide to long data for c3d points
+#'
+#' Convert from wide representation of data (3 columns per point) to long data
+#' (1 column per point) in c3d point data
+#'
+#' @param x A data.frame with c3d point data with three columns (x, y, z) per
+#'   point.
+#'
+#' @return A data.frame with one column per point. It has n*3 rows
+#'   and k columns, with n as the number of recorded frames and k as the
+#'   number of recorded points.
+#' @export
+c3d_longer <- function(x) {
+  # get new column names
+  old_names <- colnames(x)[0:(ncol(x) / 3 - 1) * 3 + 1]
+  new_names <- sub("_([xyz])$", "", old_names)
+
+  # get split factors for reshape
+  splt <- split(seq_len(ncol(x)), rep(1:(ncol(x) / 3), each = 3))
+  r <- reshape(
+    data = x,
+    varying = splt,
+    v.names = new_names,
+    direction = "long",
+    timevar = "type",
+    times = c("x","y","z"),
+    idvar = "frame"
+  )
+  # reorder columns
+  r <- r[,c(ncol(r), seq_along(r)[-ncol(r)])]
+  # reorder rows
+  r[order(r$frame, r$type),]
 }
 
 #' Get analog data from a c3d file
