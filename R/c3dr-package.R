9b6b47bb48f385762749d4e7e16ff374942e9e87
Simon Nolte <81624911+smnnlt@users.noreply.github.com>
2024-09-10 14:51:56 +0200

    Create high-level package documentation

--- R/c3dr-package.R
+++ R/c3dr-package.R
@@ -1,3 +1,14 @@
+#' @keywords internal
+#'
+#' @section Main functions:
+#' \itemize{
+#'   \item Use \code{\link{c3d_read}} for the import of C3D data.
+#'   \item Use \code{\link{c3d_data}} and \code{\link{c3d_analog}} for assessing the
+#'   point and the analog data.
+#' }
+"_PACKAGE"
+
+
 ## usethis namespace: start
 #' @importFrom Rcpp sourceCpp
 #' @useDynLib c3dr, .registration = TRUE
