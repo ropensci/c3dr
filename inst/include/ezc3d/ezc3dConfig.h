11dce72620fa35853776c9719d12076e02f43ba3
Simon Nolte <81624911+smnnlt@users.noreply.github.com>
2025-01-21 14:23:12 +0100

    Update to new ezc3d version new version: 1.5.18

--- inst/include/ezc3d/ezc3dConfig.h
+++ inst/include/ezc3d/ezc3dConfig.h
@@ -3,21 +3,28 @@
 
 // dllexport/import declaration
 #ifdef EZC3D_SHARED
-    #ifdef _WIN32
-        #ifdef EZC3D_API_EXPORTS
-            #define EZC3D_API __declspec(dllexport)
-        #else
-            #define EZC3D_API __declspec(dllimport)
-        #endif
-    #else
-        #define EZC3D_API __attribute__ ((visibility ("default")))
-    #endif
+#ifdef _WIN32
+#ifdef EZC3D_API_EXPORTS
+#define EZC3D_API __declspec(dllexport)
 #else
-    #define EZC3D_API
+#define EZC3D_API __declspec(dllimport)
+#endif
+#else
+#define EZC3D_API
+#endif
+#else
+#define EZC3D_API
+#endif
+
+#ifdef _WIN32
+#define EZC3D_VISIBILITY
+#else
+#define EZC3D_VISIBILITY __attribute__((visibility("default")))
 #endif
 
 // Write down the version of the ezc3d
-#define EZC3D_VERSION "@EZC3D_VERSION_MAJOR@.@EZC3D_VERSION_MINOR@.@EZC3D_VERSION_PATCH@"
+#define EZC3D_VERSION                                                          \
+  "@EZC3D_VERSION_MAJOR@.@EZC3D_VERSION_MINOR@.@EZC3D_VERSION_PATCH@"
 #define EZC3D_CONTACT "pariterre@hotmail.com"
 
 #endif // EZC3D_CONFIG_H
